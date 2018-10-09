5605fc0972e2fd4da4a8232fa6f4a31c44f854c6
anthony8887 <stokesw@b16925.local>
2018-10-09 14:49:45 -0600

    Update 1

---
+++ warren/functions.js
@@ -0,0 +1,15 @@
+//function multiplier_thing(number1,number2) {
+   // let new_number = number1 * number2;
+  //  return new_number;
+//}
+//console.log(multiplier_thing(5,8))
+
+//var some_number = 5;
+//var Dan = multiplier_thing(some_number);
+
+//console.log(Dan);
+
+function email_check(email) {
+    if (email)
+  return `Nice ${email} let's see whats new`;
+}
