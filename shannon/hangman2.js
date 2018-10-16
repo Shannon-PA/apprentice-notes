993247693ceb7c2276761b438b2628916491e303
Shannon Buys <shannon.buys@pinnacol.com>
2018-10-16 10:13:29 -0600

    Started a hangman2 file to work with apprentices on

---
+++ shannon/hangman2.js
@@ -0,0 +1,19 @@
+const answer = "BULL";
+
+function guess() {
+  let letter;
+  // put logic here
+  return letter
+}
+
+function check(letter, answer) {
+  let correct;
+  // put logic here, should return true or false;
+  return correct;
+}
+
+
+
+
+
+
