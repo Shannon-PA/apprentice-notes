29dbd8a882d623920a52f3ce0283368ffaace164
Marco Knight <knightm@b19879.local>
2018-10-25 11:53:03 -0600

    updated FFC

---
+++ Marco/hangman.js
@@ -0,0 +1,20 @@
+const answer = "CRAZY";
+let letters_tired = ["A", "B"]
+let current_solution = "_____"
+
+function guess() {
+    let letter;
+    // put logic here
+    return letter
+  }
+  
+  function check(letter, answer) {
+    let correct;
+    // put logic here, should return true or false;
+    return correct;
+  }
+  
+  function getCurrentSolution() {
+    return currentSolution;
+  }
+  
