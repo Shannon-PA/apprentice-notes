b4c3375228a47d8f6b25772ec5b7f58468b31c84
Brielynn Garza <garzab@b18959.local>
2018-11-01 11:59:28 -0600

    no new progress for fcc

--- brie/hangman.js
+++ brie/hangman.js
@@ -1,18 +1,60 @@
+const readline = require('readline-sync');
 const answer = "COCONUT";
-let letters_tried= ["A", "B"]
-letters_tried.push
+let letters_tried= [];
+const max_tries = 6;
+let current_solution = "____";
+
 function guess(){
-    let current_solution = "CO_____"
-    console.log(letters_tried)
-} 
-    function check(letter, answer) { 
-    let correct;
+   var letter = readline.question("pick a letter.");
+   console.log(letter);
+   return letter;
+
+}  
+
+function check(letter, answer) { 
+let correct;
+for (let i=0; i< answer.length;i++){
+    $('#list option').each(function(index){
+}
+return correct;
+}
+//log each index to the answer
     
-    return correct;
-    }
+function check_win_conditions (answer, current_solution) {
+    let won = true;
+    //put my logic here
+    return won 
+}
+function check_Loss_conditions ( ){
+    let loss = false;
+}
+function updateLettersTried (){
+    let = []
+}
+
+function updateProgress (){
     
-    
-    function check_win_conditions (answer, current_solution) {
-    
-    }
-    
+}
+
+    // what does it need to know to do its job
+    //what are we going to get back from it?
+
+//     var bar ="whatsup "
+//      var foo = readline.question('What is your name?');
+//      console.log(bar + foo +"!");
+ guess () 
+
+
+
+while(run_game){
+let letter = guess();
+let correct = check(letter, answer);
+
+if (correct){
+console.log('Placeholder: YOU GOT IT RIGHT!')
+} else {
+console.log('Placeholder: YOU GOT IT WRONG')
+}
+}
+
+console.log(1==1)
