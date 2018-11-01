f35f24f33175927e10251f7146d6d80f1c62b7b8
Marco Knight <knightm@b19879.local>
2018-11-01 10:33:21 -0600

    updated FCC and Hangman

--- Marco/hangman.js
+++ Marco/hangman.js
@@ -1,20 +1,42 @@
+const readline = require('readline-sync')
+
 const answer = "CRAZY";
-let letters_tired = ["A", "B"]
-let current_solution = "_____"
+let letters_tired = [];
+let current_solution = ""
+const max_tries = 6;
+
 
 function guess() {
     let letter;
-    // put logic here
-    return letter
-  }
+    // Ask user for letter, return letter
+    letter = readline.question('Choose a letter please.')
+    return letter;
+}
   
-  function check(letter, answer) {
-    let correct;
-    // put logic here, should return true or false;
-    return correct;
-  }
+function check(letter, answer) {
+let correct;
+// // put logic here, should return true or false;
+return correct;
+}
   
-  function getCurrentSolution() {
-    return currentSolution;
-  }
-  
+function update_solution(letter, answer, current_solution) {
+    let updated_solution
+    //put logic here
+    return update_solution;
+}
+
+function update_letter_tried (letter, letters_tried){
+    let new_letters_tried;
+    //put logic here
+    return new_letters_tried;
+}
+
+function check_game_over (letters_tried, current_solution, answer, max_tries) {
+    let game_over;
+    //put logic here
+    return game_over;
+
+}
+
+guess()
+
