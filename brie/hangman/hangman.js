8e0e5f3b5e6dbcc199c5387cdb71929d5eea0437
Brielynn Garza <garzab@b18959.local>
2018-11-08 11:57:18 -0700

    No new updates for FCC

--- brie/hangman/hangman.js
+++ brie/hangman/hangman.js
@@ -3,21 +3,22 @@ const answer = "COCONUT";
 let letters_tried= [];
 const max_tries = 6;
 let current_solution = "____";
-let run_game = true;
 
 function guess(){
    var letter = readline.question("pick a letter.");
    console.log(letter);
    return letter;
+
 }  
 
-function check( letter, answer ) { 
-    let correct;
-    for (let i=0; i< answer.length;i++) {
-
-    }
-    return correct;
+function check(letter, answer) { 
+let correct;
+for (let i=0; i< answer.length;i++){
+    $('#list option').each(function(index){
 }
+return correct;
+}
+//log each index to the answer
     
 function check_win_conditions (answer, current_solution) {
     let won = true;
@@ -37,30 +38,23 @@ function updateProgress (){
 
     // what does it need to know to do its job
     //what are we going to get back from it?
+
 //     var bar ="whatsup "
 //      var foo = readline.question('What is your name?');
 //      console.log(bar + foo +"!");
-console.log("Place greeting here")
+ guess () 
+
+
 
 while(run_game){
-    // output current solution
-    // output wrong guesses
-    let letter = guess();
-    let correct = check(letter, answer);
+let letter = guess();
+let correct = check(letter, answer);
 
-    if (correct){
-    console.log('Placeholder: YOU GOT IT RIGHT!')
-    // call updateProgress()
-    } else {
-    console.log('Placeholder: YOU GOT IT WRONG')
-    // update letters tried with wrong letter
-    }
-    // if (check_win_conditions()) {
-        // output congrats
-        // run_game = false;
-    // }
-    // if (check_loss_conditions()) {
-        // output failure message
-        // run_game = false;
-    // }
+if (correct){
+console.log('Placeholder: YOU GOT IT RIGHT!')
+} else {
+console.log('Placeholder: YOU GOT IT WRONG')
 }
+}
+
+console.log(1==1)
