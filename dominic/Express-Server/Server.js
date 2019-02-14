66bf717167f22fc707fd4eb09bacffaed50b4180
Dominic Martinez <martindo@b18827.local>
2019-02-14 16:38:10 -0700

    new hw

--- dominic/Express-Server/Server.js
+++ dominic/Express-Server/Server.js
@@ -2,6 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Wassup!'))
+function auth(req,res, next) {
+    console.log(req.headers.boi)
+    next();
+}
+
+function handler(req,res,next){
+    res.send("Your dad")
+}
+
+app.get('/', auth,handler)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+//app.get('/', (req, res) => res.send('Wassup!'))
+
+//app.listen(port, () => console.log(`Example app listening on port ${port}!`))
