e63407e8a345ae18dd0e20567110c90df1f67874
Brielynn Garza <garzab@b18959.local>
2019-02-26 08:06:39 -0700

    Homework

--- brie/express-server/server.js
+++ brie/express-server/server.js
@@ -4,10 +4,28 @@ const port = 3000
 
 function auth(req, res, next) {
     console.log(req.headers.water)
-  next();
+    if (req.headers.water =='cooking') {
+        next();
+    } else {
+        res.status(401);
+        res.send("GET OUT!!!!!!")
+    }
 }
+
 function handler(req, res, next){
-    res.send('Yaga Yo');
+    const list = [
+        {
+            todo: "die",
+            completed: false
+        },
+        {
+            todo: "Live",
+            completed: true   
+        },
+    ];
+    res.status(200);
+    res.json(list);
+
 }
 app.get('/', auth, handler) 
 
