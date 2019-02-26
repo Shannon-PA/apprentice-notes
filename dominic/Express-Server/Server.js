50885d2e814227dad08eb51b09d4100e087cf7b4
Dominic Martinez <martindo@b18827.local>
2019-02-26 08:17:14 -0700

    server HW

--- dominic/Express-Server/Server.js
+++ dominic/Express-Server/Server.js
@@ -3,19 +3,39 @@ const app = express()
 const port = 3000
 
 function auth(req,res, next) {
-    console.log(req.headers.boi)
+  console.log(req.headers.boi);
+  if (req.headers.boi == 'chicken') {
     next();
+  } else {
+    res.status(401);
+    res.send("get your bubblegum looking head out of here boooooiiiiii")
+  }
 }
 
 function handler(req,res,next){
-    res.send("Your dad")
+  const list = [
+    {
+      todo: "punch billy",
+      completed: true
+    },
+    {
+      todo: "stare at wall",
+      ongoing: true
+    },
+    {
+      todo: "punch you in the face",
+      completed: false 
+    },
+      {
+        todo: "reach into the darkest depths of oblivion to personally gain contact with hermaeus mora himself for infinite knowlege and power of the universe",
+      completed: true
+
+    },
+  ];
+    res.status(200);
+    res.json(list);
 }
 
 app.get('/', auth,handler)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-
-//app.get('/', (req, res) => res.send('Wassup!'))
-
-//app.listen(port, () => console.log(`Example app listening on port ${port}!`))
